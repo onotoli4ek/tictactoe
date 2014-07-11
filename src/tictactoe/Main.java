a565267b42137458ffcd28b0e4e144525b1f1073
Syvaschenko <onotoli4ek@gmail.com>
2014-07-11 15:05:31 +0300

    changed initialCheckField in class Computer

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         Field field = new Field();
+        //test
+        System.out.println("test");
         IOHelper ioHelper = new IOHelper();
         Computer comp = new Computer();
         while (!field.isGameOver()) {
